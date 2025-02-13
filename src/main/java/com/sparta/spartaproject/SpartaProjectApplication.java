957841a4f21df25778c7681d5206455cc4d4cd47
leedongha1998 <leedongha98@gmail.com>
2025-02-13 12:56:09 +0900

    init commit

---
+++ src/main/java/com/sparta/spartaproject/SpartaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaProjectApplication.class, args);
+    }
+
+}
