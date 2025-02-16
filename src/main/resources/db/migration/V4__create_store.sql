a4f971e9fcaf29777036da065c7f159f91a94694
juneunbae <dmsqo1403@naver.com>
2025-02-17 00:43:55 +0900

    fix: flyway 수정
    1. User
    - 참조 테이블명 변경 (user -> p_user)
    
    2. V2__create_user.sql
    - dev.user -> dev.p_user
    
    3. V4__create_store.sql
    - owner_id, category_id 외래키 제거
    - is_deleted, deleted_at 추가
    
    4. V5__create_order.sql
    
    5. V6__create_order_history.sql

--- src/main/resources/db/migration/V4__create_store.sql
+++ src/main/resources/db/migration/V4__create_store.sql
@@ -1,19 +1,17 @@
 CREATE TABLE dev.p_stores (
       id      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-      owner_id      BIGINT          NOT NULL REFERENCES p_users(id),
-      category_id   UUID            NOT NULL REFERENCES p_categories(id),
-
+      owner_id      BIGINT          NOT NULL,
+      category_id   UUID            NOT NULL,
       name          VARCHAR(50)     NOT NULL,
       address       VARCHAR(300)    NOT NULL,
       status        VARCHAR(20)     NOT NULL DEFAULT 'BEFORE_OPEN',
-
       tel           VARCHAR(20)     NOT NULL,
       description   VARCHAR(255),
-
       open_time     TIME            NOT NULL,
       close_time    TIME            NOT NULL,
       closed_days   VARCHAR(20),
-
+      is_deleted    BOOL DEFAULT FALSE,
+      deleted_at    TIMESTAMP,
       created_at    TIMESTAMP,
       created_by    BIGINT,
       updated_at    TIMESTAMP,
