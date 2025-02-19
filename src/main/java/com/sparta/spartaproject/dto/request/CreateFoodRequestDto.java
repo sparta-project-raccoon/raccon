a4a0f684d52255f9311de5d846a584849882cb3f
Yeonnnny <kdoy030@gmail.com>
2025-02-19 12:25:53 +0900

    fix : DTO tab 정렬

--- src/main/java/com/sparta/spartaproject/dto/request/CreateFoodRequestDto.java
+++ src/main/java/com/sparta/spartaproject/dto/request/CreateFoodRequestDto.java
@@ -5,10 +5,10 @@ import com.sparta.spartaproject.domain.food.Status;
 import java.util.UUID;
 
 public record CreateFoodRequestDto (
-        UUID storeId,
-        String name,
-        Integer price,
-        String description,
-        Status status
+    UUID storeId,
+    String name,
+    Integer price,
+    String description,
+    Status status
 ){
 }
--- src/main/java/com/sparta/spartaproject/dto/request/UpdateFoodRequestDto.java
+++ src/main/java/com/sparta/spartaproject/dto/request/UpdateFoodRequestDto.java
@@ -3,9 +3,9 @@ package com.sparta.spartaproject.dto.request;
 import com.sparta.spartaproject.domain.food.Status;
 
 public record UpdateFoodRequestDto (
-        String name,
-        Integer price,
-        String description,
-        Status status
+    String name,
+    Integer price,
+    String description,
+    Status status
 ){
 }
