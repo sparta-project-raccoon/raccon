70035889b75acf898230deb7ff5cdea74ddbc5c6
Yeonnnny <kdoy030@gmail.com>
2025-02-23 12:07:56 +0900

    fix : 음식 기능 수정

--- src/main/java/com/sparta/spartaproject/domain/food/FoodService.java
+++ src/main/java/com/sparta/spartaproject/domain/food/FoodService.java
@@ -210,7 +210,6 @@ public class FoodService {
                     .stream()
                     .findFirst()
                     .orElse(null);
-
                 log.info("Image url: {}", imageUrl);
                 return foodMapper.toFoodDetailDto(food, imageUrl);
             }).toList(),
