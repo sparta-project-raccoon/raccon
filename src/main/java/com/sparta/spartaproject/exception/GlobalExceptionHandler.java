6f404a564dea7171125ccac7dd1e1d1483ce53c1
leedongha1998 <leedongha98@gmail.com>
2025-02-24 11:17:04 +0900

    fix : 안쓰는 import 제거

--- src/main/java/com/sparta/spartaproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/spartaproject/exception/GlobalExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
